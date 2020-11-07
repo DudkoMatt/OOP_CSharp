79a4c87c7245b5a6d25ab40abd76c0f81ddf68ac
DudkoMatt <github.dudko.matt@mail.ru>
2020-11-07 14:02:30 +0300

    Lab_5: chore: Created new project, added description

---
+++ Lab_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
