95c2f2debb3c94796f0f7593677edd02671f255e
DudkoMatt <github.dudko.matt@mail.ru>
2020-12-05 14:26:27 +0300

    Lab_5: feat: Added Client.cs

---
+++ Lab_5/Client.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab_5
+{
+    public class Client
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Address { get; private set; }
+        public string PassportNumber { get; private set; }
+
+        public Client(string name, string surname, string address = null, string passportNumber = null)
+        {
+            Name = name;
+            Surname = surname;
+            Address = address;
+            PassportNumber = passportNumber;
+        }
+
+        public void UpdateName(string name) => Name = name;
+        public void UpdateSurname(string surname) => Surname = surname;
+        public void UpdateAddress(string address) => Address = address;
+        public void UpdatePassport(string passportNumber) => PassportNumber = passportNumber;
+    }
+}
